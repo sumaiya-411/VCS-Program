363f75f2ec1fc28bc8c4d6b4301b72f3606f518e
Sumaiya Kouser <misssumaiya411@gmail.com>
2025-04-22 13:09:49 +0530

    initial commit

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+   
+    fib_sequence = [0, 1]
+    
+   
+    for i in range(2, n):
+       
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    
+   
+    return fib_sequence
+
+
+print(fibonacci(10))
